5fdfb68406418f0d08d712f2e39f60efbefe9b19
Gus Caplan <me@gus.host>
2017-03-28 16:42:23 -0500

    formatting

--- src/util/checkEventLoop.js
+++ src/util/checkEventLoop.js
@@ -2,6 +2,6 @@ setInterval(() => {
   const last = process.hrtime();
   setImmediate(() => {
     const delta = process.hrtime(last)[1];
-    if (delta > 5e8) process.stdout.write(`---- EVENT LOOP BLOCKED FOR ${delta / 1000}! ----`);
+    if (delta > 500e6) process.stdout.write(`---- EVENT LOOP BLOCKED FOR ${delta / 1e8}! ----`);
   });
 }, 500);
